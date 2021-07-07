3077e57fb01ed361e1aecfde75e836b8f7e8fa04
Zubin Khavarian <210675+zubinkhavarian@users.noreply.github.com>
2021-07-07 15:52:53 -0400

    Minor updates
    Signed-off-by: Zubin Khavarian <210675+zubinkhavarian@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-import Calculator from './Calculator';
+import Calculator from "./Calculator";
 
 // https://www.youtube.com/watch?v=ZtU7Mhf9vN8
 // https://codepen.io/mjijackson/pen/xOzyGX
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {});
+
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
 
 export default App;
